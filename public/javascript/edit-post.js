0f20a18d282da7ba78d86fbd84720899655995a0
jcaswell <jakecaswell88@gmail.com>
2021-11-29 12:56:22 -0500

    added the front end js for creating, editing, and deleting posts. Also added in the front end js to create comments on the posts

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,28 @@
+async function editPost(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const blog_text = document.querySelector('input[name="post-body"]').value.trim();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ]
+
+    const response = await fetch(`/api/post/${id}`, {
+        method: 'PUT', 
+        body: JSON.stringify({
+            post_id: id,
+            title, 
+            content
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    })
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText)
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editPost);
